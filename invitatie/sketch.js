53e301b94ef034d551dabb639529d447d42ae955
Rusu83 <35503591+Rusu83@users.noreply.github.com>
2019-01-07 19:57:40 +0200

    Update sketch.js

--- invitatie/sketch.js
+++ invitatie/sketch.js
@@ -1,9 +1,39 @@
+var r,g,b,rf,gf,bf,p,sx=0,sy=0;
 
 function setup() {
   createCanvas(window.innerWidth,window.innerHeight);
-  frameRate(5);
+  r=random(255);
+  g=random(255);
+  b=random(255);
+  rf=random(255);
+  gf=random(255);
+  bf=random(255);
+  p=int(random(4));
+  noStroke();
+  //frameRate(5);
 }
 
 function draw() {
-  background(random(255),random(255),random(255));
+  background(r,g,b);
+  fill(rf,bf,gf);
+  switch(p%5){
+    case 0:rect(0,0,sx,sy);break;
+    case 1:rect(width,0,-sx,sy);break;
+    case 2:rect(width,height,-sx,-sy);break;
+    case 3:rect(0,height,sx,-sy);break;
+		case 4:ellipse(width/2,height/2,2*sx,2*sy);
+  }
+ sx+=width/20;
+ sy+=height/20;
+ if(sx>width&&sy>height){
+		sx=0;
+	 	sy=0;
+	 r=rf;
+	 g=gf;
+	 b=bf;
+	 	rf=random(255);
+  gf=random(255);
+  bf=random(255);
+  p=int(random(5));
+ }
 }
